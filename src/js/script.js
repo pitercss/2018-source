492d07ef362affcc692748929070f0a67c11b3a5
Elena Esina <hellemess@yandex.ru>
2018-01-29 12:22:50 +0300

    универсализировала галерею, совместила блоки галереи и слайдера, убрала фантомную ссылку, исправила ошибки с отступами, внесла исправления в код оживления страницы

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-window.slider = (() => {
+window.script = (() => {
   const ENTER = 13;
 
   // добавление обработчика нажатия мышки или клавиши enter
@@ -17,25 +17,27 @@ window.slider = (() => {
 
   const sessionToggles = document.querySelectorAll('.session__toggle');
 
-  const toggleSession = (evt) => {
-    evt.preventDefault();
-    evt.target.parentElement.classList.toggle('session--closed');
-    evt.target.classList.toggle('session__toggle--hide');
-    evt.target.setAttribute('aria-label', evt.target.getAttribute('aria-label') === 'Open talk description' ? 'Close talk description' : 'Open talk description');
-  };
+  if (sessionToggles.length > 0) {
+    const toggleSession = (evt) => {
+      evt.preventDefault();
+      evt.target.parentElement.classList.toggle('session--closed');
+      evt.target.classList.toggle('session__toggle--hide');
+      evt.target.setAttribute('aria-label', evt.target.getAttribute('aria-label') === 'Open talk description' ? 'Close talk description' : 'Open talk description');
+    };
 
-  const turnOnToggles = (nodeList, i) => {
-    addHandler(nodeList[i], toggleSession);
-  };
+    const turnOnToggles = (nodeList, i) => {
+      addHandler(nodeList[i], toggleSession);
+    };
 
-  sessionToggles.forEach((it, i) => {
-    turnOnToggles(sessionToggles, i);
-  });
+    sessionToggles.forEach((it, i) => {
+      turnOnToggles(sessionToggles, i);
+    });
+  }
 
   // слайдер
 
   const slider = document.querySelector('.gallery');
-  const images = slider.querySelectorAll('.gallery__image');
+  const images = slider.querySelectorAll('.gallery__item');
 
   let currentImageIndex = 0;
 
@@ -43,14 +45,14 @@ window.slider = (() => {
     evt.preventDefault();
     if (evt.target.classList.contains('gallery__button--prev')) {
       if (currentImageIndex > 0) {
-        images[currentImageIndex].classList.remove('gallery__image--show');
-        images[currentImageIndex - 1].classList.add('gallery__image--show');
+        images[currentImageIndex].classList.remove('gallery__item--show');
+        images[currentImageIndex - 1].classList.add('gallery__item--show');
         currentImageIndex--;
       }
     } else if (evt.target.classList.contains('gallery__button--next')) {
       if (currentImageIndex < images.length - 1) {
-        images[currentImageIndex].classList.remove('gallery__image--show');
-        images[currentImageIndex + 1].classList.add('gallery__image--show');
+        images[currentImageIndex].classList.remove('gallery__item--show');
+        images[currentImageIndex + 1].classList.add('gallery__item--show');
         currentImageIndex++;
       }
     }
@@ -61,26 +63,32 @@ window.slider = (() => {
   // открытие блока со стикерами
 
   const preview = document.querySelector('.preview');
-  const stickers = preview.querySelector('.preview__stickers-list');
-  const link = preview.querySelector('.preview__link');
 
-  const turnOnStickers = (evt) => {
-    evt.preventDefault();
-    stickers.classList.toggle('preview__stickers-list--show');
-    evt.target.innerHTML = evt.target.innerHTML === 'Add Sticker' ? 'Close' : 'Add Sticker';
-  };
+  if (preview !== null) {
+    const stickers = preview.querySelector('.preview__stickers-list');
+    const link = preview.querySelector('.preview__link');
 
-  addHandler(link, turnOnStickers);
+    const turnOnStickers = (evt) => {
+      evt.preventDefault();
+      stickers.classList.toggle('preview__stickers-list--show');
+      evt.target.innerHTML = evt.target.innerHTML === 'Add Sticker' ? 'Close' : 'Add Sticker';
+    };
+
+    addHandler(link, turnOnStickers);
+  }
 
   // закрытие справки о цене
 
   const priceInfo = document.querySelector('.price-info');
-  const close = priceInfo.querySelector('.price-info__close');
 
-  const closeInfo = (evt) => {
-    evt.preventDefault();
-    priceInfo.style.display = 'none';
+  if (priceInfo !== null) {
+    const close = priceInfo.querySelector('.price-info__close');
+
+    const closeInfo = (evt) => {
+      evt.preventDefault();
+      priceInfo.style.display = 'none';
+    }
+
+    addHandler(close, closeInfo);
   }
-
-  addHandler(close, closeInfo);
 })();
